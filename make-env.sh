c98bfefcbd127b3190874ba16e4e538284137d33
Ned Letcher <nletcher@gmail.com>
2020-11-11 23:30:40 +1100

    improve presentation

--- make-env.sh
+++ make-env.sh
@@ -6,7 +6,7 @@ VENV=melbviz
 pyenv virtualenv ${PYTHON} ${VENV}
 export PYENV_VERSION=${VENV}
 
-pip install -U pip wheel pip-tools
+pip install -U pip wheel
 pip install -r requirements.txt
 pip install -e .
 
@@ -14,5 +14,8 @@ jupyter labextension install @pyviz/jupyterlab_pyviz
 jupyter labextension install @jupyter-widgets/jupyterlab-manager plotlywidget
 jupyter labextension install jupyterlab-plotly
 jupyter labextension install @ryantam626/jupyterlab_code_formatter
+jupyter nbextension install --py hide_code
+jupyter nbextension enable --py hide_code
+jupyter serverextension enable --py hide_code
 
 unset PYENV_VERSION
