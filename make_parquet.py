8992c91a871eebe5aa75f35acab20fd7466c6204
Ned Letcher <nletcher@gmail.com>
2021-04-26 09:06:56 +1000

    fix path variables

--- make_parquet.py
+++ make_parquet.py
@@ -3,5 +3,5 @@
 from melbviz.pedestrian import PedestrianDataset
 from melbviz.config import MELBVIZ_DATA_PATH, MELBVIZ_COUNTS_CSV_PATH, MELBVIZ_SENSOR_CSV_PATH
 
-data = PedestrianDataset.load(COUNTS_CSV_PATH, sensor_csv_path=SENSOR_CSV_PATH)
-data.to_parquet(DATA_PATH / "melbviz.parquet")
+data = PedestrianDataset.load(MELBVIZ_COUNTS_CSV_PATH, sensor_csv_path=MELBVIZ_SENSOR_CSV_PATH)
+data.to_parquet(MELBVIZ_DATA_PATH / "melbviz.parquet")
