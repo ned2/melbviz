97de009430c9a18d9a134bbb9e3f2bcbb9cc9fc7
Ned Letcher <nletcher@gmail.com>
2021-05-03 18:47:44 +1000

    Fix Jupyter Lab and Jupyter Notebook extension installation

--- make-env.sh
+++ make-env.sh
@@ -11,11 +11,10 @@ pip install -r requirements.txt
 pip install -e .
 
 #jupyter labextension install @pyviz/jupyterlab_pyviz
-jupyter labextension install @jupyter-widgets/jupyterlab-manager plotlywidget
-jupyter labextension install jupyterlab-plotly
+jupyter labextension install jupyterlab-plotly plotlywidget
 jupyter labextension install @ryantam626/jupyterlab_code_formatter
-jupyter nbextension install --py hide_code
-jupyter nbextension enable --py hide_code
+jupyter nbextension install --user --py hide_code
+jupyter nbextension enable --user --py hide_code
 jupyter serverextension enable --py hide_code
 
 unset PYENV_VERSION
