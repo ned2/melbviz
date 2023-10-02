9dbe506ef3b8b28865b223a2e8cfb076e0149fb9
Ned Letcher <nletcher@gmail.com>
2023-10-02 22:56:28 +1100

    standardise on same env var for data path

--- src/melbviz/config.py
+++ src/melbviz/config.py
@@ -7,11 +7,11 @@ MAPBOX_KEY = os.getenv("MELBVIZ_MAPBOX_KEY")
 MELBVIZ_DATA_PATH = Path(os.getenv("MELBVIZ_DATA_PATH", Path.home() / "melbviz_data"))
 
 MELBVIZ_COUNTS_CSV_PATH = (
-    MELBVIZ_DATA_PATH / "Pedestrian_Counting_System_-_Monthly__counts_per_hour_.csv"
+    MELBVIZ_DATA_PATH / "Pedestrian_Counting_System_Monthly_counts_per_hour_may_2009_to_14_dec_2022.csv"
 )
 
 MELBVIZ_SENSOR_CSV_PATH = (
-    MELBVIZ_DATA_PATH / "Pedestrian_Counting_System_-_Sensor_Locations.csv"
+    MELBVIZ_DATA_PATH / "pedestrian-counting-system-sensor-locations.csv"
 )
 
 MELBVIZ_CLEANED_DATA_PATH = MELBVIZ_DATA_PATH / "melbviz.parquet"
